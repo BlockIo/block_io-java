6918ce440ad8c8719756ff53572e52bbbb1fd416
Wasiq Nazir <nwasiq@gmail.com>
2021-06-24 23:31:45 +0500

    added json files for create and sign transaction

--- src/test/java/lib/TransactionTest.java
+++ src/test/java/lib/TransactionTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.libdohj.params.LitecoinTestNet3Params;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
